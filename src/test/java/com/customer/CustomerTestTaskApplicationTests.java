fa9c64717c5744bacbb8f0dee948dad8aff537d8
sviat-h <svyatoslav.grinik@gmail.com>
2022-05-03 15:05:36 +0300

    added main spring boot classes

---
+++ src/test/java/com/customer/CustomerTestTaskApplicationTests.java
@@ -0,0 +1,11 @@
+package com.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerTestTaskApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
